efb95e909f881cff7d5483eb10ea77e8b2adb076
Will <Will@169.254.237.83>
2017-11-09 23:39:23 -0500

    Create Database

--- src/Database.java
+++ src/Database.java
@@ -1,9 +1,45 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Database {
+	private ArrayList<DataManager> mdata;
 	
 	public Database()
 	{
-		
+		mdata = new ArrayList<>();
 	}
-
+	public void addDataBase(DataManager data)
+	{
+		mdata.add(data);
+	}
+	public void removeDataBase(DataManager data)
+	{
+		boolean found = false;
+		int index = 0;
+		Iterator<DataManager> it = mdata.iterator();
+		while(it.hasNext() && !found)
+		{
+			if(it.next().equals(data))
+			{
+				mdata.remove(index);
+				found = true;
+			}
+			index++;
+		}
+	}
+	public void listAllMovies()
+	{
+		Iterator<DataManager> it = mdata.iterator();
+		if(DataManager.getNumberOfMovies()<=0)
+		{
+		while(it.hasNext())
+		{
+			it.next().printMoviesInOrder();
+		}
+		}
+		else
+		{
+			System.out.println("ERROR NO Movies");
+		}
+	}
 }
